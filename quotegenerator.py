b19c97a4debe47f6622f621adac16f327f408c58
Miles Ty <milestywork@gmail.com>
2023-09-19 10:53:47 -0700

    Added some new notes and Python files

---
+++ quotegenerator.py
@@ -0,0 +1,18 @@
+# Quote Generator
+# Author: Miles Ty
+# Date: 19 September 2023
+
+# Output a quote from someome famous
+print("Do or do not. There is no try. --Yoda")
+
+# Print out another quote 
+print("You must be the change you wish to see in the world. --Mahatma Gandhi")
+
+# Print out another, different quote 
+print("The only thing we have to fear is fear itself. --Franklin D. Roosevelt")
+
+# Ask the user a question
+input("What was yout favourite quote?")
+
+# Respond with something a human would say
+print("Oh, that's my favourite too")

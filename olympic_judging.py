54c02e0fc05868ad00f62cc13ec9a7b47ae201d8
Miles Ty <milestywork@gmail.com>
2023-11-06 11:42:43 -0800

    added a fix to make it ask the question five times

--- olympic_judging.py
+++ olympic_judging.py
@@ -5,7 +5,11 @@ NUM_RESPONDENTS = 5
 # Greet the user
 print("Please answer the following question")
 # Create a list of questions to ask
-for _ in range(NUM_RESPONDENTS): questions = ["On a scale of 1 to 10 how were they?"]
+for _ in range(NUM_RESPONDENTS): questions = ["On a scale of 1 to 10 how were they?",
+                                              "On a scale of 1 to 10 how were they?",
+                                              "On a scale of 1 to 10 how were they?",
+                                              "On a scale of 1 to 10 how were they?",
+                                              "On a scale of 1 to 10 how were they?"]
 # For every question in that list, ask a question, get the user's rating, and add the rating for the total-rating
 
 total_rating = 0

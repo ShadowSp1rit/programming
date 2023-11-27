a88b1cb6b806f606dc756f6083c56ec945b773cc
Miles Ty <milestywork@gmail.com>
2023-11-27 11:19:56 -0800

    commit and push still unfinished

---
+++ fuctions_pratice_questions.py
@@ -0,0 +1,49 @@
+# Functions Questions #1-4
+# Author: Miles Ty
+# Date: Nov 27th, 2023
+
+
+# Question 1
+
+# Write a function called stars()
+
+# Make the function take an int as a parameter.
+
+# It then returns a string of stars with the same length as the argument
+
+def row_of_stars(num_stars: int) -> None:
+    """Calculates the number of stars.
+    Results are in one line
+    
+    Params:
+    
+    sidelength - length of one row of the square"""
+
+    stars("*")
+    
+    return
+print("How many stars would you like?")
+stars =input()
+print(f"{row_of_stars}")
+
+# Question 2
+
+# Write a function that TAKES THREE NUMBERS
+
+# it should RETURN THE BIGGEST of the three numbers
+
+# Call it "biggest_of_three"
+
+def biggest_of_three(numbers: float) -> None:
+    """Calculates the highest number out of three inputs.
+    Result is the highest number.
+    
+    Params:
+    
+    numbers - integers"""
+
+    biggest = numbers <= numbers
+
+    return
+
+    print(f"The highest number is {biggest_of_three}")

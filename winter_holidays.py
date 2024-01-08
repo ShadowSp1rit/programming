aea01bd6838327c54fd1e00e962a6497ecbf4c36
Miles Ty <milestywork@gmail.com>
2024-01-08 10:52:19 -0800

    completed winter_holidays

---
+++ winter_holidays.py
@@ -0,0 +1,35 @@
+# Winter Holidays Review
+# Miles Ty
+# Jan 8th, 2024
+
+# Requirements:
+# - create a function called winter_holdiays()
+# - take one parameter
+# - string
+# - return a summary of an event from your holidays
+
+import random
+
+
+def winter_holiday(good_or_bad: str) -> str:
+    """Give a summary of our winter holidays 2023/24
+    
+    Params:
+        good_or_bad - indicate what kind of event to summarize
+        
+    Returns:
+        an event that happened during the holidays
+        the event is selected randomly"""
+    
+    return good_or_bad
+
+def main() -> None:
+    print(winter_holiday("went to Osaka, experiencing a tour / explore sites"))
+    print(winter_holiday("visited phillipines to see cousins, and relax"))
+     
+
+if __name__ == "__main__":
+    main() # describes hidden properties, do what's inside of this codeblock
+
+
+

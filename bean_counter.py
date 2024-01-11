d436922a687226cff6b247c355887330f31b32ee
Miles Ty <milestywork@gmail.com>
2024-01-11 11:09:55 -0800

    commit

--- bean_counter.py
+++ bean_counter.py
@@ -6,13 +6,22 @@
 #  - Count red pixels/beans
 #  - Report on the percentage of all red beans
 
+# Version 0.2
+# - Count green pixels/beans
+# - Report on percentage of all the green beans
+
 from PIL import Image
 
 import colour_helper
 
+RED_PIXEL = (160, 0, 0)
+GREEN_PIXEL = (0, 160, 0)
+
+
 jelly_bean_img = Image.open("./Images/Jelly Beans.jpg")
 
 red_pixels = []
+green_pixels = [] # Where the green pixels will be stored
 
 # Visit every pixel in the image
 for y in range(jelly_bean_img.height):
@@ -24,18 +33,36 @@ for y in range(jelly_bean_img.height):
         if colour_helper.pixel_to_name(current_pixel) == "red":
             # Store its location somewhere
             red_pixels.append((x, y))
+        elif colour_helper.pixel_to_name(current_pixel) == "green":
+            # Store its location somewhere
+            green_pixels.append((x, y))
 
 # Create a map of all red pixels "found"
 # Create a new image that stores the map
+#   - the new image should be the same size as original
+orig_image_width = jelly_bean_img.width
+orig_image_height = jelly_bean_img.height
+
+green_pixel_map = Image.new("RGB", (orig_image_width, orig_image_height))
+
+# For every pixel location in the red_pixels list
+# Place a red pixel at that location
+for pixel_loc in red_pixels:
+    green_pixel_map.putpixel(pixel_loc, GREEN_PIXEL)
+
+# Save the image
+green_pixel_map.save("./Images/green_pixel_map.jpg")
+green_pixel_map.close()
 
 # Count all the locations of red pixels
-red_pixel_count = len(red_pixels)
+green_pixel_count = len(red_pixels)
 total_pixels = jelly_bean_img.width * jelly_bean_img.height
 
 # Divide by the total pixels in the image
-red_pixel_percentage = red_pixel_count / total_pixels * 100
+green_pixel_percentage = green_pixel_count / total_pixels * 100
 
 # Generate the report
-print(f"Red Jelly Beans: {round(red_pixel_percentage, 2)}%")
+print(f"Green Jelly Beans: {round(green_pixel_percentage, 2)}%")
 
 jelly_bean_img.close()
+

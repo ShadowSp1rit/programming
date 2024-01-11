6a489e5255741166b8a9885f1dc28ec900c6602c
Miles Ty <milestywork@gmail.com>
2024-01-11 11:43:09 -0800

    commit and push

--- bean_counter.py
+++ bean_counter.py
@@ -7,8 +7,12 @@
 #  - Report on the percentage of all red beans
 
 # Version 0.2
-# - Count green pixels/beans
-# - Report on percentage of all the green beans
+#  - Count green pixels/beans
+#  - Report on the %age of all green beans
+
+# Version 0.3
+# - Count blue pixels/beans
+# - Report on the percentage of all blue beans
 
 from PIL import Image
 
@@ -16,12 +20,13 @@ import colour_helper
 
 RED_PIXEL = (160, 0, 0)
 GREEN_PIXEL = (0, 160, 0)
-
+BLUE_PIXEL = (0, 0, 160)
 
 jelly_bean_img = Image.open("./Images/Jelly Beans.jpg")
 
 red_pixels = []
-green_pixels = [] # Where the green pixels will be stored
+green_pixels = []
+blue_pixels = []
 
 # Visit every pixel in the image
 for y in range(jelly_bean_img.height):
@@ -29,12 +34,10 @@ for y in range(jelly_bean_img.height):
         # Get the pixel information
         current_pixel = jelly_bean_img.getpixel((x, y))
 
-        # If this pixel is red
+        # Detect the current pixel's colour
         if colour_helper.pixel_to_name(current_pixel) == "red":
-            # Store its location somewhere
             red_pixels.append((x, y))
-        elif colour_helper.pixel_to_name(current_pixel) == "green":
-            # Store its location somewhere
+        elif colour_helper.pixel_to_name(current_pixel) == "jelly bean green":
             green_pixels.append((x, y))
 
 # Create a map of all red pixels "found"
@@ -43,26 +46,30 @@ for y in range(jelly_bean_img.height):
 orig_image_width = jelly_bean_img.width
 orig_image_height = jelly_bean_img.height
 
-green_pixel_map = Image.new("RGB", (orig_image_width, orig_image_height))
+blue_pixel_map = Image.new("RGB", (orig_image_width, orig_image_height))
 
 # For every pixel location in the red_pixels list
 # Place a red pixel at that location
-for pixel_loc in red_pixels:
-    green_pixel_map.putpixel(pixel_loc, GREEN_PIXEL)
+for pixel_loc in green_pixels:
+    blue_pixel_map.putpixel(pixel_loc, BLUE_PIXEL)
 
 # Save the image
-green_pixel_map.save("./Images/green_pixel_map.jpg")
-green_pixel_map.close()
+blue_pixel_map.save("./Images/green_pixel_map.jpg")
+blue_pixel_map.close()
 
 # Count all the locations of red pixels
-green_pixel_count = len(red_pixels)
+red_pixel_count = len(red_pixels)
+green_pixel_count = len(green_pixels)
+blue_pixel_count = len(blue_pixels)
 total_pixels = jelly_bean_img.width * jelly_bean_img.height
 
 # Divide by the total pixels in the image
+red_pixel_percentage = red_pixel_count / total_pixels * 100
 green_pixel_percentage = green_pixel_count / total_pixels * 100
+blue_pixel_percentage = blue_pixel_count / total_pixels * 100
 
 # Generate the report
+print(f"Red Jelly Beans: {round(red_pixel_percentage, 2)}%")
 print(f"Green Jelly Beans: {round(green_pixel_percentage, 2)}%")
-
+print(f"Blue Jelly Beans: {round(blue_pixel_percentage, 2)}%")
 jelly_bean_img.close()
-

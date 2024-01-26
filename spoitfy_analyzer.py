a0603a99e3d7ca7d7f3db2ef0965248f4190e936
Miles Ty <milestywork@gmail.com>
2024-01-26 10:58:19 -0800

    save and commit

--- spoitfy_analyzer.py
+++ spoitfy_analyzer.py
@@ -3,31 +3,26 @@
 # Jan 16 2024
 
 # Version 0.1
-# - From the data set, get all the songs
-# - performed by Drake
+#   - From the data set, get all the songs
+#     performed my Drake
 
 import csv
 
 
 # Open up the file
-with open("spotify.csv") as f:
-    # ----- Look for all songs performed by Drake
-    #       using linear search
+with open("./spotify.csv") as f:
+    # ---- Look for all songs performed by Drake
+    #      Use linear search
     # Create a csv reader object
     csv_reader = csv.DictReader(f)
 
     # Create a list to store all Drake's songs
     drake_songs = []
-    # Create a counter for the current line number
-
 
     # for every song in the .csv file
     for line in csv_reader:
-        print(line)
-
-        # if this song  is sung by Drake
-        if "drake" in line["artist"].lower():    
-            # add it to the Drake list
+        if "drake" in line["artist"].lower():
+            # add it to the Drake's songs list
             drake_songs.append(
                 (line["artist"], line["song_title"], line["danceability"])
             )
@@ -35,10 +30,28 @@ with open("spotify.csv") as f:
 for song in drake_songs:
     print(song)
 
-# Print out all songs that have
-    # a danceability of >+ 0.6
+# --- Sorting Algorithm (lowest to highest)
+# --- Selection Sort
 
+# Starting at the beginning of the list moving to the end
+for i in range(len(drake_songs)):
+    # Set the current item to the smallest value
+    smallest_danceability = drake_songs[i][-1]
+    smallest_index = i
 
+    # For the rest of the list
+    for j in range(i + 1, len(drake_songs)):
+        # Check to see if this is smaller
+        if drake_songs[j][-1] < smallest_danceability:
+            smallest_danceability = drake_songs[j][-1]
+            smallest_index = j
+
+    # Swap the current position with the smallest number we found
+    drake_songs[i], drake_songs[smallest_index] = (
+        drake_songs[smallest_index],
+        drake_songs[i],
+    )
+
+print("--- Sorted Drake Songs by Danceability")
 for song in drake_songs:
-    if float(song[-1]) >= 0.6:
-        print(song)
+    print(f"{song[1]}:\t\t{song[2]}")
